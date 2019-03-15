6040a667ea0895f628c2e06d1d2bb2c55e43681c
vephior <48455516+vephior@users.noreply.github.com>
2019-03-15 15:56:41 +0100

    Add files via upload

---
+++ register-thanks.php
@@ -0,0 +1,22 @@
+
+<!doctype html>
+<html lang=eng>
+<head>
+<title>Register thank you page</title>
+<meta charset=utf-8>
+<link rel="stylesheet" type="text/css" href="includes.css">
+</head>
+<body>
+<div id="container">
+<?php include("header.php"); ?>
+<?php include("nav.php"); ?>
+<?php include("info-col.php"); ?>
+<div id="content"><!--Start of the the thank you page-content. -->
+<h2> Thank you for registereing</h2>
+<p>The thank you page content. The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.The thank you page content.</p>
+<!--end of the thank you page-content. -->
+</div>
+<div>
+<?php include("footer.php"); ?>
+</body>
+</html>
