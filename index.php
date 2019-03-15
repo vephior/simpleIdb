5d984ae55b3b35b1f42352509fb5614631e007be
vephior <48455516+vephior@users.noreply.github.com>
2019-03-15 13:38:24 +0100

    Add files via upload
    Dit is de originele file van de site

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <!doctype html>
 <html lang=en>
 <head>
-<title>Template for an interactive web page</title>
+<title>Index-register</title>
 <meta charset=utf-8>
 <link rel="stylesheet" type="text/css" href="includes.css">
 </head>
 <body>
-<div id='container'>
-<?php include('header.php'); ?>
-<?php include('nav.php'); ?>
-<?php include('info-col.php'); ?>
-<div id='content'><!--Start of page content.-->
+<div id="container">
+<?php include("header.php"); ?>
+<?php include("nav.php"); ?>
+<?php include("info-col.php"); ?>
+<div id="content"><!-- Start of content for home page -->
 <h2>This is the Home Page</h2>
-<p> The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content.The homepage content. </p>
-<!--End of the homepage content.-->
-</div>
-</div>
-<?php include('footer.php'); ?>
+<p>The home page content. The home page content. The home page content. The home page content. The home page content. <br>The home page content. The home page content. The home page content. The home page content. <br>The home page content. The home page content. <br>The home page content. The home page content. The home page content. </p>
+</div><!-- End content. -->
+</div>	
+<?php include("footer.php"); ?>
 </body>
 </html>
- 
