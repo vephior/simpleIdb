aa49cfecac3e10ee7ff6cdb28808c61370efa5ee
vephior <48455516+vephior@users.noreply.github.com>
2019-03-15 13:33:36 +0100

    Add files via upload
    originele code gedownload van site

--- nav.php
+++ nav.php
@@ -1,22 +1,23 @@
 <style type="text/css">
-ul {position:absolute; top:190px; left:-10px; color:navy;
-width:135px; text-align:center; margin:0; }
+ul { position:absolute; top:190px; left:-10px; color:navy; width:135px; text-align:center; margin:0;
+}
 /* set general side button styles */
-li {width:115px; list-style-type :none; margin-bottom: 3px; text-align: center;
+li { width:115px; list-style-type :none; margin-bottom: 3px; text-align: center;
 }
 /* set general anchor styles */
-li a {display: block; width: 115px; color: white; font-weight: bold; text-decoration:none
+li a { display: block; width:115px; color: white; font-weight: bold; text-decoration: none
 }
 /* specify state styles. */
 /* mouseout (default) */
-li a {background: #5B78BE; border: 4px outset #aabaff;
+li a { background: #5B78BE; border: 4px outset #aabaff;
 }
 /* mouseover */
-li a:hover {display:block; background: #0a4adf; border: 4px outset #8abaff; width:115px;
-}/* onmousedown */
-li a:active {background:#aecbff; border: 4px inset #aecbff;
+li a:hover { display:block; background: #0a4adf; border: 4px outset #8abaff; width:115px;
+}
+/* onmousedown */
+li a:active { background:#aecbff; border: 4px inset #aecbff;
 </style>
-<div id="nav"><!--The side menu column contains the vertical menu block-->
+<div id="nav"><!--This side menu column contains the vertical menu-->
 <ul>
 <li><a href="page-2.php" title="Page two">Page 2</a></li>
 <li><a href="page-3.php" title="Page three">Page 3</a></li>
